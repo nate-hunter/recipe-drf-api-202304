5cd89910271262ea7db84a29b885d3caaa54e3c8
Nate Hunter <nhunter@indemand.com>
2023-05-15 13:04:48 -0400

    setup + defined initial test for User admin page

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,40 @@
+"""
+Tests for the Django admin modifications.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
+
+    def setUp(self):
+        """Create user and client."""
+
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpwd123',
+        )
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpwd123',
+            name='User Test',
+        )
+
+    def test_user_lists(self):
+        """Test that users are listed on the page."""
+
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
+
+
