aba165fb455b7b827aeaef9910432170dce5c170
Nate Hunter <nhunter@indemand.com>
2023-05-16 10:32:02 -0400

    implemented token api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,7 +2,9 @@
 Serializers for the User API View.
 """
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+
+from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
@@ -19,3 +21,31 @@ class UserSerializer(serializers.ModelSerializer):
         """Create + return a new User with encrypted password."""
 
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for User auth token."""
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Valide + authenticate User."""
+
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
