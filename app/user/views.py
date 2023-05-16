326c567f138308c3e99aa14926001d03755bc186
Nate Hunter <nhunter@indemand.com>
2023-05-16 08:30:32 -0400

    implemented ability to create new user in serializers/views/urls

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""
+Views for the User API.
+"""
 
-# Create your views here.
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user."""
+
+    serializer_class = UserSerializer
