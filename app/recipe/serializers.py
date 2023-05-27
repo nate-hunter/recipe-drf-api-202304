a85051c763a2018b9d670b15ae019fda8f17a269
Nate Hunter <nhunter@indemand.com>
2023-05-26 22:50:53 -0400

    implemented listing Ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,7 +4,7 @@ Serializers for Recipe APIs.
 
 from rest_framework import serializers
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -12,6 +12,15 @@ class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for Ingredients."""
+
+    class Meta:
+        model = Ingredient
         fields = ['id', 'name']
         read_only_fields = ['id']
 
